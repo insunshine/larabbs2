e87f65b43a1a532b966763909da1f737626332cc
insunshine <292228108@qq.com>
2018-03-29 09:50:31 +0000

    test

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -19,8 +19,16 @@
             </ul>
             <!-- 右侧导航栏-->
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="#">登录</a> </li>
-                <li><a href="#">注册</a> </li>
+                @guest
+                    <li><a href="{{ route('login') }}">登录</a> </li>
+                    <li><a href="{{ route('register') }}">注册</a> </li>
+                @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+
+                        </a>
+                    </li>
+                @endguest
             </ul>
         </div>
     </div>
